c86ed2f5b8480f7cbd092ffd410d8b0a2c6656a4
Antonio Eduardo <antonioeduardo1998@gmail.com>
2020-10-16 14:22:55 -0300

    cache providers

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,35 +1,55 @@
-import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
-import Appointment from "../infra/typeorm/entities/Appointment";
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-    provider_id: string;
-    day: number;
-    month: number;
-    year: number;
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
 }
-
 
 @injectable()
 class ListProviderAppointmentsService {
-    constructor(
-        @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository
-    ) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
 
-    public async execute({ provider_id, month, year, day }: IRequest): Promise<Appointment[]> {
-        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-            provider_id,
-            year,
-            month,
-            day
-        })
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
 
-        return appointments;
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
+    );
+
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      );
+
+      console.log('Buscou do banco')
+
+      await this.cacheProvider.save(cacheKey, appointments);
     }
+
+    return appointments;
+  }
 }
 
 export default ListProviderAppointmentsService;
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepositories';
 import {inject, injectable} from 'tsyringe';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface Request {
     name: string;
@@ -18,7 +19,10 @@ class CreateUserService {
         private usersRepository: IUsersRepository,
 
         @inject('HashProvider')
-        private hashProvider: IHashProvider
+        private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider
         ) {}
 
     public async execute({ name, email, password }: Request): Promise<User> {
@@ -37,6 +41,8 @@ class CreateUserService {
             email,
             password: hashedPassword,
         });
+
+        await this.cacheProvider.invalidatePrefix('providers-list');
 
         return user;
     }
